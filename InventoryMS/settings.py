93cb23881ee96fbc547903e67b46b4d108672495
kk-25 <259421@student.pwr.edu.pl>
2026-01-06 14:14:12 -0500

    files changed for azure deployment v2

--- InventoryMS/settings.py
+++ InventoryMS/settings.py
@@ -141,7 +141,7 @@ LOGOUT_URL = 'logout'
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR,'static')
 ]
-STATIC_ROOT = os.path.join(BASE_DRI, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
 MEDIA_URL = '/images/'
 
